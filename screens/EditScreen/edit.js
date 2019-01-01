e3d86bc8bfe5e13e93ffe339d520ef6b3dc866b1
Pedro Galetti <pedro.galetti@mazzafc.tech>
2019-01-01 14:07:19 -0200

    editScreen

---
+++ screens/EditScreen/edit.js
@@ -0,0 +1,28 @@
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    return fn();
+  }
+  return document.addEventListener('DOMContentLoaded', fn);
+}
+
+ready(() => {
+  document.querySelector('#home').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../index.html';
+  });
+  
+  document.querySelector('#register').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../RegisterScreen/register.html';
+  });
+  
+  document.querySelector('#edit').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = './edit.html';
+  });
+  
+  document.querySelector('#refresh').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../RefreshScreen/refresh.html';
+  });
+});
