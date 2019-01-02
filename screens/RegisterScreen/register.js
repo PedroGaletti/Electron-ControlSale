5bb7489a6c06fad9ac556bff4a4b15f3eb1463a8
Pedro Galetti <pedro.galetti@mazzafc.tech>
2019-01-02 18:47:54 -0200

    fix register combo and edit combo

--- screens/RegisterScreen/register.js
+++ screens/RegisterScreen/register.js
@@ -6,35 +6,12 @@ function ready(fn) {
 }
 
 ready(() => {
-  var plus = document.getElementsByClassName("plus-li");
-  var minus = document.getElementsByClassName("minus-li");
-  var i, j;
-  for (i = 0; i < plus.length; i++) {
-    plus[i].onclick = function() {   
-      this.classList.add('minus-li');
-      this.classList.remove('plus-li');
-      this.lastChild.classList.remove('icon-plus');
-      this.lastChild.classList.add('icon-minus')
-      document.querySelector('#product_combos').appendChild(this);
-    };
-  }
-
-  for (j = 0; j < minus.length - 1; j++) {
-    minus[i].onclick = function() {
-      this.classList.add('plus-li');
-      this.classList.remove('minus-li');
-      this.lastChild.classList.remove('icon-minus');
-      this.lastChild.classList.add('icon-plus');
-      document.querySelector('#products').appendChild(this);
-    };
-  }
-
   if (document.querySelector('#product_radio').hasAttribute('checked')) {
     document.querySelector('#form-product').style.display = 'block';
     document.querySelector('#form-combo').style.display = 'none';
     document.querySelector('#form-body').reset();
   }
-
+  
   document.querySelector('#home').addEventListener('click', (e) => {
     e.preventDefault();
     window.location = '../index.html';
