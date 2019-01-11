c893454233cac730ee061acf66abff6da5639a44
Pedro Galetti <pedro.galetti@mazzafc.tech>
2019-01-11 01:44:36 -0200

    fix Menu import

---
+++ 288Worship-win32-ia32/resources/app/screens/RegisterScreen/register.js
@@ -0,0 +1,34 @@
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    return fn();
+  }
+  return document.addEventListener('DOMContentLoaded', fn);
+}
+
+ready(() => {
+  if (document.querySelector('#product_radio').hasAttribute('checked')) {
+    document.querySelector('#form-product').style.display = 'block';
+    document.querySelector('#form-combo').style.display = 'none';
+    document.querySelector('#form-body').reset();
+  }
+  
+  document.querySelector('#home').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../index.html';
+  });
+
+  document.querySelector('#register').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = './register.html';
+  });
+
+  document.querySelector('#edit').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../ListProductScreen/listProduct.html';
+  });
+
+  document.querySelector('#refresh').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = '../RefreshScreen/refresh.html';
+  });
+});
